cf4caee534aebaba08739545761f27e0c60d0819
moreal <dev.moreal@gmail.com>
2019-03-26 09:38:07 +0900

    Add simple code and structure

---
+++ run.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/hwangseonu/gin-restful"
+	"net/http"
+)
+
+type HelloWorld struct {
+	*gin_restful.Resource
+}
+
+func (r HelloWorld) Get(Id int) (gin.H, int) {
+	return gin.H{"id": Id}, http.StatusOK
+}
+
+func (r HelloWorld) Get(Id string) (gin.H, int) {
+	return gin.H{"id": Id}, http.StatusOK
+}
+
+func main() {
+	router := gin.Default()
+	api := gin_restful.NewApi(router,"/")
+	api.AddResource(
+		HelloWorld{gin_restful.InitResource()}, "/")
+	router.Run(":7000")
+}
