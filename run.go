6d22657d4bfbf9626e5eeebf73c11531f1473393
moreal <dev.moreal@gmail.com>
2019-04-02 13:01:42 +0900

    Upload wip status

--- run.go
+++ run.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"github.com/moreal/boj-vs-code-api-server/server"
-	"google.golang.org/appengine"
 )
 
 func main() {
 	s := server.CreateServer()
 	s.Run(":8080")
-	appengine.Main()
 }
